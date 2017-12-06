5df439ebd1202f8d6324f5b3b7d682ddffb480d1
tamiroze <tamir.rozenberg@instacart.com>
2017-12-06 11:47:58 -0800

    Update stv_recents.sql
    adding kill session using pid

--- stv_recents.sql
+++ stv_recents.sql
@@ -1,4 +1,7 @@
 --see recent redshift queries
 select *
 from stv_recents
-where status='Running'
+where status='Running';
+
+--kill sesseion using PID from stv_recents
+select pg_terminate_backend(121815);
